402098c2c75bc93cf54c0facc8062260c596b6fd
Sam Soffes <sam@soff.es>
2020-06-17 22:59:05 -0700

    Version 1.0.4

--- components/download_button.js
+++ components/download_button.js
@@ -28,7 +28,7 @@ const DownloadButton = (props) => {
 }
 
 DownloadButton.defaultProps = {
-  "url": "https://github.com/nothingmagical/ZoomIn/releases/download/v1.0.3/ZoomIn-1.0.3.zip"
+  "url": "https://github.com/nothingmagical/ZoomIn/releases/download/v1.0.4/ZoomIn-1.0.4.zip"
 }
 
 export default DownloadButton
