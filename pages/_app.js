13bd93abefb09dc446f1fb958436a7ae1ec6c054
Sam Soffes <sam@soff.es>
2021-01-11 07:02:50 -0800

    Remove Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
 import React, { useEffect } from 'react';
 import Router from 'next/router';
-import * as Fathom from 'fathom-client';
-
-// Record a pageview when route changes
-Router.events.on('routeChangeComplete', () => {
-  Fathom.trackPageview();
-});
 
 function App({ Component, pageProps }) {
-  // Initialize Fathom when the app loads
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      Fathom.load();
-      Fathom.setSiteId('JUBUFFLK');
-      Fathom.trackPageview();
-    }
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
