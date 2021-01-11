acfb466a0add14a7e1332f45af0e53bf6e18cb31
Sam Soffes <sam@soff.es>
2021-01-11 07:04:29 -0800

    Add note it's not maintained

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default () => (
   <Page>
     <Subheading>Safari extension to automatically open Zoom links</Subheading>
 
+    <p><strong>Update:</strong> Zoom keeps changing their page which breaks Zoom In. Since I no longer use Zoom, Zoom In isn’t maintained any more. The code is still <a href="https://github.com/soffes/ZoomIn">on GitHub</a> if you want to mess with it though.</p>
+
     <p>Are you Zooming all the time? Are you tired of telling Safari it’s okay to open Zoom links in Zoom? Have I got the app for you!</p>
 
     <p>Zoom In is an app that installs a Safari extension. This extension does two things. <mark>It automatically opens Zoom links in the Zoom app.</mark> If that works, it will automatically close the tab so you don’t have tons of ”Opening in Zoom…“ tabs left in your browser.</p>
